7b746a6e02c2f82e4eba0dfbff6301594fc34cac
fazeelamushtaq <fazeela.mushtaq.sirius@gmail.com>
2025-09-15 23:54:14 +0500

    Fix UI Changes

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -2,13 +2,13 @@ import { FC } from 'react'
 import 'remixicon/fonts/remixicon.css'
 
 const ButtonModel = {
-    primary: "bg-blue-400 hover:bg-blue-500 rounded font-medium text-white px-6 py-2",
-    secondary: "bg-indigo-400 hover:bg-indigo-500 rounded font-medium text-white px-6 py-2",
-    danger: "bg-rose-400 hover:bg-rose-500 rounded font-medium text-white px-6 py-2",
-    warning: "bg-amber-400 hover:bg-amber-500 rounded font-medium text-white px-6 py-2",
-    dark: "bg-zinc-400 hover:bg-zinc-500 rounded font-medium text-white px-6 py-2",
-    success: "bg-green-400 hover:bg-green-500 rounded font-medium text-white px-6 py-2",
-    info: "bg-cyan-400 hover:bg-cyan-500 rounded font-medium text-white px-6 py-2"
+    primary: "bg-blue-400 hover:bg-blue-500 rounded font-medium text-white px-6 py-2 cursor-pointer",
+    secondary: "bg-indigo-400 hover:bg-indigo-500 rounded font-medium text-white px-6 py-2 cursor-pointer",
+    danger: "bg-rose-400 hover:bg-rose-500 rounded font-medium text-white px-6 py-2 cursor-pointer",
+    warning: "bg-amber-400 hover:bg-amber-500 rounded font-medium text-white px-6 py-2 cursor-pointer",
+    dark: "bg-zinc-400 hover:bg-zinc-500 rounded font-medium text-white px-6 py-2 cursor-pointer",
+    success: "bg-green-400 hover:bg-green-500 rounded font-medium text-white px-6 py-2 cursor-pointer",
+    info: "bg-cyan-400 hover:bg-cyan-500 rounded font-medium text-white px-6 py-2 cursor-pointer"
 }
 
 interface ButtonInterface {
